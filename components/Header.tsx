daa686de117d3743489d131496fecdbaf7d56da6
Danny <dannydotdev@gmail.com>
2025-04-18 22:16:31 +0100

    File structure

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-[#2F1F14]">
+      <Image
+        src="/images/logo.svg"
+        alt="logo"
+        width={100}
+        height={100}
+        className="mx-auto py-4"
+      />
+    </div>
+  );
 };
 
 export default Header;
