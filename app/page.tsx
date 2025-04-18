97451a4c1df6a4c86754e2f2eccdddc24e94f44a
Danny <dannydotdev@gmail.com>
2025-04-18 23:24:26 +0100

    Design the hero secton

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Discover from "@/components/Discover";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Reviews from "@/components/Reviews";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <Hero />
       <Discover />
       <Reviews />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,55 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { Galada, Pompiere } from "next/font/google";
+
+const galada = Galada({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const pompiere = Pompiere({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const links = [
+  { name: "Home", href: "/", id: 1 },
+  { name: "About", href: "/discover", id: 2 },
+  { name: "Reviews", href: "/reviews", id: 3 },
+  { name: "Contact", href: "/contact", id: 4 },
+];
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div
+      className="relative w-full h-[500px] p-4 bg-cover bg-right"
+      // style={{
+      //   backgroundImage: "url('/images/bg-header.svg')",
+      // }}
+    >
+      <section className="flex items-center">
+        <Image src="/images/logo.svg" alt="logo" width={200} height={200} />
+
+        <div className="bg-[#6F4436] px-3 py-1 flex items-center gap-5 z-10">
+          {links.map((link) => (
+            <Link href={link.href} key={link.id} className="text-white">
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <p className={`${pompiere.className} text-5xl`}>Coffee Makes Mood</p>
+        <p className={`${galada.className} font-bold text-white text-5xl`}>
+          "Indulge in Every Sip: Experience the Richness of Coffee!"
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default Hero;
